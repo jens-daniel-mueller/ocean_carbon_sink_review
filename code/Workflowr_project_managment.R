cd99ccda6e4cd1d098cd89105f0f9a811f2c88a1
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-05-13 16:35:24 +0200

    dCgasex included

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -115,7 +115,7 @@ wflow_publish(here::here(
 message = "use setup child Rmd file")
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "beta plots refined")
+wflow_publish(all = TRUE, message = "dCgasex included")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "include G19 comparison", republish = TRUE)
