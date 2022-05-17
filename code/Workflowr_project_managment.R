79d942730eed8ea7dfb4e78a14de59b3f21361db
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-05-17 16:30:57 +0200

    added some uncertainty estimates

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -115,7 +115,7 @@ wflow_publish(here::here(
 message = "use setup child Rmd file")
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "dCgasex included")
+wflow_publish(all = TRUE, message = "added some uncertainty estimates")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "include G19 comparison", republish = TRUE)
