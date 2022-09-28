9590d16275f8cfdcb0754726b573b8eb219b4a7c
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-09-28 10:31:41 +0200

    cleaned script

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -115,7 +115,7 @@ wflow_publish(here::here(
 message = "use setup child Rmd file")
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "split total and delta Cant")
+wflow_publish(all = TRUE, message = "cleaned script")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "include G19 comparison", republish = TRUE)
