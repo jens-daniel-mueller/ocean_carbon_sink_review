d6c72a99db2512eccc5d863634a1a4c8bc72e326
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-10-07 10:12:21 +0200

    rebuild with new Rstudio version

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -115,7 +115,7 @@ wflow_publish(here::here(
 message = "use setup child Rmd file")
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "use NOAA GML atm pCO2")
+wflow_publish(all = TRUE, message = "rebuild with new Rstudio version")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "include G19 comparison", republish = TRUE)
